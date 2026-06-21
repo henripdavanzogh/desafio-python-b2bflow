ac804a96d67bf4e622a26c65adf74112156b8d29
henripdavanzogh <henrique.davanzo0202@gmail.com>
2026-06-21 19:27:55 -0300

    refactor: deleted class Settings

--- config/config.py
+++ config/config.py
@@ -2,13 +2,10 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv()
+# supabase
+SUPABASE_API_URL = os.getenv("SUPABASE_API_URL")
+SUPABASE_API_KEY = os.getenv("SUPABASE_API_KEY")
 
-
-class Settings:
-    # supabase
-    SUPABASE_API_URL = os.getenv("SUPABASE_API_URL")
-    SUPABASE_API_KEY = os.getenv("SUPABASE_API_KEY")
-
-    # zapi
-    ZAPI_INSTANCE_ID = os.getenv("ZAPI_INSTANCE_ID")
-    ZAPI_TOKEN = os.getenv("ZAPI_TOKEN")
+# zapi
+ZAPI_INSTANCE_ID = os.getenv("ZAPI_INSTANCE_ID")
+ZAPI_TOKEN = os.getenv("ZAPI_TOKEN")
