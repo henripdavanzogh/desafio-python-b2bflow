b9bc152437736de8a5ef718d7a589431162ff58b
henripdavanzogh <henrique.davanzo0202@gmail.com>
2026-06-22 14:16:14 -0300

    refactor: split main.py into services/supabase_client.py and services/zapi_client.py

--- main.py
+++ main.py
@@ -1,41 +1,9 @@
 from dotenv import load_dotenv
-from supabase import create_client
-import os
-import requests
+from services.supabase_client import get_contacts
+from services.zapi_client import send_message
 
 load_dotenv()
 
-
-# SUPABASE
-def get_contacts():
-    supabase = create_client(
-        supabase_key=os.getenv("SUPABASE_API_KEY"),
-        supabase_url=os.getenv("SUPABASE_API_URL"),
-    )
-
-    response = supabase.table("contatos").select("*").limit(3).execute()
-    return response.data
-
-
-# ZAPI
-def send_message(name, phone):
-    message = f"Olá, {name} tudo bem com você?"
-
-    INSTANCE_ID = os.getenv("ZAPI_INSTANCE_ID")
-    TOKEN = os.getenv("ZAPI_INSTANCE_TOKEN")
-    CLIENT_TOKEN = os.getenv("ZAPI_CLIENT_TOKEN")
-
-    # request data
-    url = f"https://api.z-api.io/instances/{INSTANCE_ID}/token/{TOKEN}/send-text"
-    payload = {"phone": phone, "message": message}
-    headers = {"Client-Token": CLIENT_TOKEN}
-    requests.post(url=url, json=payload, headers=headers)
-
-
 table_contacts = get_contacts()
 for contact in table_contacts:
-    send_message((contact["name"], contact["phone_number"]))
-
-
-if __name__ == "__main__":
-    get_contacts()
+    send_message(contact["name"], contact["phone_number"])
