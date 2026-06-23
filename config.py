97d9913ba5146c8d483a3eba705b354a51451ac0
henripdavanzogh <henrique.davanzo0202@gmail.com>
2026-06-22 23:56:35 -0300

    feat: handling valueerrors for key-value

--- config.py
+++ config.py
@@ -9,3 +9,14 @@ SUPABASE_KEY = os.getenv("SUPABASE_API_KEY")
 INSTANCE_ID = os.getenv("ZAPI_INSTANCE_ID")
 TOKEN = os.getenv("ZAPI_INSTANCE_TOKEN")
 CLIENT_TOKEN = os.getenv("ZAPI_CLIENT_TOKEN")
+
+if not SUPABASE_URL:
+    raise ValueError("SUPABASE_API_URL não configurada")
+elif not SUPABASE_KEY:
+    raise ValueError("SUPABASE_API_KEY não configurada")
+elif not INSTANCE_ID:
+    raise ValueError("ZAPI_INSTANCE_ID não configurada")
+elif not TOKEN:
+    raise ValueError("ZAPI_INSTANCE_TOKEN não configurada")
+elif not CLIENT_TOKEN:
+    raise ValueError("ZAPI_CLIENT_TOKEN não configurada")
